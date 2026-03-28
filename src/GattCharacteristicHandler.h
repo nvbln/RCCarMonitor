4c2488a74840b4270a31c148693db3f3d8852939
Nathan van Beelen <nathan@vanbeelen.site>
2026-03-28 09:04:17 +0300

    Add docs to GattCharacteristicHandler

--- src/GattCharacteristicHandler.h
+++ src/GattCharacteristicHandler.h
@@ -7,7 +7,15 @@
 
 #include <memory>
 
-// TODO: Add documentation.
+/**
+ * @class GattCharacteristicHandler
+ * @brief Safely handles the GattCharacteristic in Bluetooth BLE.
+ *
+ * The GattCharacteristics on Bluetooth BLE are not guaranteed
+ * to always be available. This results in a need to safely handle
+ * the characteristic. As multiple characteristics are used throughout
+ * the program, this handler encapsulates all the safety mechanisms.
+ */
 class GattCharacteristicHandler : public IGattCharacteristicHandler {
 public:
   /**
@@ -19,17 +27,35 @@ public:
    */
   GattCharacteristicHandler(IBluetoothDevice *device, std::string characteristicId);
 
+  /**
+   * @see IGattCharacteristicHandler::isAvailable()
+   */
   bool isAvailable() const override { return mChar != nullptr; }
+
+  /**
+   * @see IGattCharacteristicHandler::onAvailable()
+   */
   void onAvailable(OnAvailableCallback callback) override { onAvailableEvent.subscribe(callback); }
+
+  /**
+   * @see IGattCharacteristicHandler::onUnavailable()
+   */
   void onUnavailable(OnUnavailableCallback callback) override {
     onUnavailableEvent.subscribe(callback);
   }
 
+  /**
+   * @see IGattCharacteristicHandler::read()
+   */
   std::optional<std::vector<uint8_t>> read() override;
+
+  /**
+   * @see IGattCharacteristicHandler::write()
+   */
   bool write(const std::vector<uint8_t> &data) override;
 
 private:
-  std::shared_ptr<IBluetoothDevice> mDevice;
+  IBluetoothDevice *mDevice;
   std::shared_ptr<IGattCharacteristic> mChar = nullptr;
   std::string mCharId;
 
